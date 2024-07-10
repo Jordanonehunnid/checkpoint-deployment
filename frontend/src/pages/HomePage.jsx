0bb3bd7514a3a6500a55f6c8edfab8f831a66d5a
Jordanonehunnid <emeka.agwu1@gmail.com>
2024-07-10 13:41:37 -0400

    package.json server.js updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
 	const getUserProfileAndRepos = useCallback(async (username = "jordanonehunnid") => {
 		setLoading(true);
 		try {
-			const res = await fetch(`https://api.github.com/users/${username}`);
+			const res = await fetch(`/api/users/profile/${username}`);
 			const { repos, userProfile } = await res.json();
 
 			repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
